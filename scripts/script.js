afabc71f6f5647faead2545ebecc27cd05322b0c
Aarnav Gill <gill.aarnav@gmail.com>
2022-11-11 10:52:48 -0800

    new java statements

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+let course1 = {
+    code:"ACIT 1620",
+    name: "fundamental web technologies"
+    };
+
+let course2 = {
+    code:"ACIT 1515",
+    name: "scripting for IT",
+     };
+
+let course3 = {
+    code:"ACIT 1630",
+    name: "Database systems",
+    };
+    
+let courseList = [course1, course2, course3];
+    
+
+input = prompt("Enter 4 digit class code");
+
+
