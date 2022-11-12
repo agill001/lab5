50a6759df562bb88bf992b47839d195e7a8d5845
Aarnav Gill <gill.aarnav@gmail.com>
2022-11-12 11:18:56 -0800

    final changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,27 @@
- let course1 = {
-   code:"1620",
-  name: "fundamental web technologies"
-    };
-
- let course2 = {
-    code:"1515",
-     name: "scripting for IT",
-     };
-
-let course3 = {
-     code:"1630",
-     name: "Database systems",
-     };
+let courseList = [{code: "ACIT 1620", name: "Fundamental web technologies"}, {code: "ACIT 1630",name: "Database Systems"}, 
+{code: "ACIT 1420",name: "Intro to systems Admin"}]
     
-let courseList = [course1, course2, course3];
-    
-let input=""
+let input=" "
 do {
- input = prompt("Enter 4 digit class code"); 
+ input = prompt("Enter your 4 digit class code"); 
  
 } while(input.length !=4 || isNaN(Number(input)));
 
-
-let classcodes = ["1620","1515","1630"]
-
-if (input in classcodes) {
-  console.log("yes I am taking the course")
+exists = false
+for (let course of courseList){
+if (course.code.includes(input)) {
+  console.log(`Yes I am taking the course: ${course.code} - ${course.name}`)
+  exists = true 
 }
-  // for (let i in classcodes){
-  //    console.log("yes i am taking the course")
-    
- else { 
-  classcodes.push(input);
-  console.log("success")
+
 }
+
+ 
+
+
+ if (exists===false) {  let newcourse = {code: "ACIT" + input, name: null}
+courseList.push(newcourse);  
+ console.log("New Course added succesfully")}
+//  console.log(courseList)
+
+
